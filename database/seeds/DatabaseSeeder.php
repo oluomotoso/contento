f2d3186c896efdd60c369977e2a9c48e579e7273
Olumide Omotoso <oluomotoso@gmail.com>
2017-04-13 18:36:51 +0100

    Signed-off-by: Olumide Omotoso <oluomotoso@gmail.com>

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,7 +11,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-         //$this->call(UsersTableSeeder::class);
+        $this->call(UsersRolesSeeder::class);
+        $this->call(UsersTableSeeder::class);
         $this->call(PlatformSeed::class);
     }
 }
