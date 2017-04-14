4d52bc90213fc1905d90328a300f84675acc0ff8
Olumide Omotoso <oluomotoso@gmail.com>
2017-04-14 15:23:12 +0100

    Signed-off-by: Olumide Omotoso <oluomotoso@gmail.com>

--- config/queue.php
+++ config/queue.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('QUEUE_DRIVER', 'sync'),
+    'default' => env('QUEUE_DRIVER', 'beanstalkd'),
 
     /*
     |--------------------------------------------------------------------------
@@ -44,7 +44,7 @@ return [
         'beanstalkd' => [
             'driver' => 'beanstalkd',
             'host' => 'localhost',
-            'queue' => 'default',
+            'queue' => 'contento',
             'retry_after' => 90,
         ],
 
